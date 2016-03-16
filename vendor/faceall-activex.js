0941c150ccda14a732eb095da0db20de1c1083e0
Jingyuan Zhu <Jingyuan Zhu>
2016-03-16 14:40:21 +0800

    again

--- vendor/faceall-activex.js
+++ vendor/faceall-activex.js
@@ -3,24 +3,25 @@
 
 <script type="text/javascript">
 
-  var video = document.getElementById("video");
-  var context = canvas.getContext("2d")
-  var errocb = function () {
+var video = document.getElementById("video");
+var context = canvas.getContext("2d")
+var errocb = function () {
                           console.log('sth wrong!');
                      }
 
-  if (navigator.getUserMedia) { // 标准API
+if (navigator.getUserMedia) { // 标准的API
                 navigator.getUserMedia({ "video": true }, function (stream) {
                     video.src = stream;
                     video.play();
                 }, errocb);
-  } else if (navigator.webkitGetUserMedia) { // WebKit 核心的API
+} else if (navigator.webkitGetUserMedia) { // WebKit 核心的API
                 navigator.webkitGetUserMedia({ "video": true }, function (stream) {
                     video.src = window.webkitURL.createObjectURL(stream);
                     video.play();
                 }, errocb);
-   }
-  document.getElementById("picture").addEventListener("click", function () {
-                context.drawImage(video, 0, 0, 400, 300);
-   });
-<script>
+ }
+
+document.getElementById("picture").addEventListener("click", function () {
+                context.drawImage(video, 0, 0, 640, 480);
+ });
+</script>
