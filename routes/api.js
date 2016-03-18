491dbf278da5e3c3ed1fc44f21447dee7d563de5
Cosmo Zhang <cosmozhang1995@gmail.com>
2016-03-18 10:05:25 +0800

    remove some schemas in api

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,6 @@ var mongoose = require('mongoose');
 var SchemaApi = require('./api/schema_api');
 
 router.use('/place', new SchemaApi(require('../lib/models/place')(mongoose)));
-router.use('/visit_history', new SchemaApi(require('../lib/models/visit_history')(mongoose)));
-router.use('/visitor', new SchemaApi(require('../lib/models/visitor')(mongoose)));
 router.use('/passport', require('./api/passport'));
 
 module.exports = router;
