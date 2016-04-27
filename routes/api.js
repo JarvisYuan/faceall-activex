b066f3f5a513a5d126c022630565c5c46df36bd3
Cosmo Zhang <cosmozhang1995@gmail.com>
2016-04-27 12:24:55 +0800

    fix bug: app not use text/json

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,14 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var SchemaApi = require('./api/schema_api');
 
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 router.use('/place', new SchemaApi(require('../lib/models/place')(mongoose)));
 router.use('/passport', require('./api/passport'));
 router.use('/common', require('./api/common'));
